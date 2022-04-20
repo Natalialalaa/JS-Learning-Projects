773eba9da1d320673f8193bd359a102f3c8425f2
Natalialalaa <priscillanatalia.id@gmail.com>
2022-04-20 23:11:10 +0200

    2nd update: add new function

--- 14_Grocery_Chart/app.js
+++ 14_Grocery_Chart/app.js
@@ -2,7 +2,7 @@
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 //<input>
-const grocery = document.querySelector('grocery');
+const grocery = document.querySelector('#grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
@@ -12,37 +12,66 @@ const clearBtn = document.querySelector('.clear-btn');
 let editElement;
 //default mode of editFlag is false
 let editFlag = false;
-let editID ="";
+let editID = "";
 
 // ****** EVENT LISTENERS **********
 form.addEventListener("submit", addItem);
 
 function addItem(e) {
+    //prevent default mode: unsubmit value to server
     e.preventDefault();
-    //.value attribute is to get the value from <input>  
+    //.value attribute is to access/get the value from <input>/line 22  
     const value = grocery.value;
-    if (value) {
-        //console.log('value is truthy');
-    }
 
     //time.getTime() will returns date as ms(number, not string) since Jan 1, 1970
+    //funny Dataset / use this methods to generate unique value/number on every submit(because we're not using any external library in this project)
     const id = new Date().getTime().toString();
 
+    //add item to list : if value is not empty and editFlag is false
     if (value && !editFlag) {
-        //console.log("add item to list");
+        //create <articles> dynamically (with class and our unique ids)
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        
+        //insert literal template within articles
+        element.innerHTML = 
+        `<p class="title">${value}</p>
+
+        <div class="btn-container">
+          <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+          <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>                
+        </div>`;
+        
+        //append child between <div class="grocery-list">
+        list.appendChild(element);
+
+        //  visibility: visible; on this new class will override :hidden; value on .grocery-container
+        container.classList.add('show-container');
+        
+        //ALERT
+        displayAlert("item added to the list", "success");
+
+    //editing item : if value is not empty and editFlag is true
     } else if (value && editFlag) {
-        //console.log("editing item");
+        console.log("editing item");
+    //there is no item : 
     } else {
-        //console.log("there is no item");
+        console.log("there is no item");
+        //ALERT
         displayAlert("please enter value", "danger");
     }
 }
+
 // ****** FUNCTIONS **********
 function displayAlert(text, action) {
+    //manipulate css text content and class to display alert -> html line 19
     alert.textContent = text;
     alert.classList.add(`alert-${action}`);
 
-    //after 1 sec, remove this alert
+    //remove this alert after 1 sec(1000 msec)
     setTimeout( () => {
         alert.textContent = "";
         alert.classList.remove(`alert-${action}`);
