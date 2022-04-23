0463cb6595fc4095b44d45d3fc200c4e42a59cf6
Natalialalaa <priscillanatalia.id@gmail.com>
2022-04-23 23:04:54 +0200

    final update: add local storage API

--- 14_Grocery_Chart/app.js
+++ 14_Grocery_Chart/app.js
@@ -1,3 +1,26 @@
+/*update and delete items from a grocery list and create a simple CRUD (Create, Read, Update, and Delete) application.
+
+CRUD plays a very important role in developing full stack applications. Without it, you wouldn't be able to do things like edit or delete posts on your favorite social media platform.  
+
+Key concepts covered:
+
+DOMContentLoaded
+new Date()
+createAttribute()
+setAttributeNode()
+appendChild()
+filter()
+map()
+localStorage.setitem/getitem object -> allows you to save key/value pairs in the browser.
+localStorage.setItem(key, value);
+
+json.stringify() is useful for, say, converting an object to a string format
+which enbales it to be sent as data to a server or for use in other 
+languages
+
+json.parse() turns a string object back into a regular object
+*/
+
 // ****** SELECT ITEMS **********
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
@@ -21,6 +44,7 @@ form.addEventListener("submit", addItem);
 //*** Clear items */
 clearBtn.addEventListener("click", clearItems);
 
+window.addEventListener('DOMContentLoaded', setupItems);
 
 function addItem(e) {
     //prevent default mode: unsubmit value to server
@@ -35,37 +59,7 @@ function addItem(e) {
     //add item to list : if value is not empty and editFlag is false
     if (value && !editFlag) {
         //create <articles> dynamically (with class and our unique ids)
-        const element = document.createElement('article');
-        element.classList.add('grocery-item');
-
-        const attr = document.createAttribute('data-id');
-        attr.value = id;
-        element.setAttributeNode(attr);
-        
-        //insert literal template within articles
-        element.innerHTML = 
-        `<p class="title">${value}</p>
-         <div class="btn-container">
-          <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
-          <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>                
-         </div>`;
-        
-    /****accessing delete and edit button by element. instead of document. (even bubbling / dynamic node), 
-         because this item is not currently exist in .html on the first load */
-        const deleteBtn = element.querySelector('.delete-btn');
-        const editBtn = element.querySelector('.edit-btn');
-
-        deleteBtn.addEventListener('click', deleteItem);
-        editBtn.addEventListener('click', editItem);
-
-    //*******************************************************************************************************/
-
-        //append element to list / <div class="grocery-list">
-        list.appendChild(element);
-
-        //  visibility: visible; on this new class will override :hidden; value on .grocery-container
-        container.classList.add("show-container");
-        
+        createListItem(id, value)
         //ALERT
         displayAlert("item added to the list", "success");
         addToLocalStorage(id, value);
@@ -77,7 +71,7 @@ function addItem(e) {
         displayAlert("value changed", "danger");
 
         //editLocalStorage
-        editLocalStorage(editID);
+        editLocalStorage(editID, value);
         setBackToDefault()
     //0 item storage/input: 
     } else {
@@ -115,6 +109,7 @@ function clearItems() {
     container.classList.remove("show-container");
     displayAlert("empty list", "danger");
     setBackToDefault();
+    localStorage.removeItem('list');
 }   
 
 //delete and edit icons
@@ -131,7 +126,7 @@ function deleteItem(e){
     }
     displayAlert('item removed', 'danger');
     setBackToDefault();
-    //removeFromLocalStorage();
+    removeFromLocalStorage(id);
 }
 
 function editItem(e){
@@ -158,16 +153,100 @@ function setBackToDefault() {
 
 
 // ****** LOCAL STORAGE **********
-function addToLocalStorage(id,value) {
-    console.log("added to local storage")
+function addToLocalStorage(id, value) {
+    //ES6 object
+    const grocery = {id, value};
+
+    let items = getlocalStorage();
+    items.push(grocery);
+
+    localStorage.setItem('list', JSON.stringify(items))
+    //console.log("added to local storage")
 }
 
 function removeFromLocalStorage(id) {
+    let items = getlocalStorage();
 
+    items = items.filter(function(item) {
+        if(item.id !== id){
+            return item;
+        }
+    })
+    localStorage.setItem('list', JSON.stringify(items));
 }
 
 function editLocalStorage(id, value) {
-
+    let items = getlocalStorage();
+    items = items.map(function(item){
+        if(item.id === id){
+            item.value = value;
+        }
+        return item;
+    })
+    localStorage.setItem('list', JSON.stringify(items));
 }
 
+function getlocalStorage() {
+    return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
+//***local storage API */
+
+
+// setItem
+// getItem
+// removeItem
+// save as strings
+//localStorage.setItem('orange', JSON.stringify(["item", "item2"]));
+//const oranges = JSON.parse(localStorage.getItem("orange"));
+//console.log(oranges);
+//localStorage.removeItem("orange");
+
+
 // ****** SETUP ITEMS **********
+
+function setupItems(){
+    let items = getlocalStorage();
+    if(items.length > 0){
+    items.forEach(function(item){
+        createListItem(item.id, item.value)
+    })
+    container.classList.add('show-container');
+    }
+}
+
+function createListItem(id, value){
+        //create <articles> dynamically (with class and our unique ids)
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        
+        //insert literal template within articles
+        element.innerHTML = 
+        `<p class="title">${value}</p>
+            <div class="btn-container">
+            <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+            <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>                
+            </div>`;
+        
+    /****accessing delete and edit button by element. instead of document. (even bubbling / dynamic node), 
+         because this item is not currently exist in .html on the first load */
+        const deleteBtn = element.querySelector('.delete-btn');
+        const editBtn = element.querySelector('.edit-btn');
+
+        deleteBtn.addEventListener('click', deleteItem);
+        editBtn.addEventListener('click', editItem);
+
+    //*******************************************************************************************************/
+
+        //append element to list / <div class="grocery-list">
+        list.appendChild(element);
+
+        //  visibility: visible; on this new class will override :hidden; value on .grocery-container
+        container.classList.add("show-container");
+}
